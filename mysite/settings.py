35a200d97b0f630c56a197fb72036be7d941b5f9
benatallah <benatallahrebai@gmail.com>
2025-05-11 00:04:00 +0100

    create class models poll , poll_option , voters and make migrations , add superuser ben , password 1234

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # my apps
-    'polls',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class poll(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    duration = models.IntegerField()
+    active = models.BooleanField(default=True)
+
+class poll_option(models.Model):
+    text = models.CharField(max_length=150)
+    voter_n = models.BigIntegerField()
+    Id_poll = models.ForeignKey(poll,on_delete=models.CASCADE)
+
+class voters(models.Model):
+    name = models.CharField(max_length=150)
+    Id_poll_option = models.ForeignKey(poll_option,on_delete=models.CASCADE)
